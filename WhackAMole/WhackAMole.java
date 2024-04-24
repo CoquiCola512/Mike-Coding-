820643c2c4e11d42096a7cfabd0d83b6ce7d334e
monke <monke@EveningStarsPC.attlocal.net>
2024-04-23 19:20:35 -0700

    Timer now only runs during game screen, takes a second to stop...

--- WhackAMole/WhackAMole.java
+++ WhackAMole/WhackAMole.java
@@ -8,10 +8,14 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.ArrayList;
 import java.util.Random;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.Timer;
 public class WhackAMole implements ActionListener, KeyListener {
 	JFrame frame = new 	JFrame();
 	JPanel panel = new JPanel();
@@ -26,6 +30,9 @@ public class WhackAMole implements ActionListener, KeyListener {
 	Font fontRestartText = new Font("Comic Sans MS", Font.BOLD, 40);
 	int[] cloudsX = new int[130];
 	int[] cloudsY = new int[130];
+	int seconds = 0;
+	boolean isOnEndState = false;
+	Timer timer; 
 	Random gen = new Random();
 public static void main(String[] args) {
 	WhackAMole mole = new WhackAMole();
@@ -33,7 +40,7 @@ public static void main(String[] args) {
 }
 
 void startUp () {
-	
+	timer = new Timer(1000, this);
 	frame.addKeyListener(this);
 	frame.setVisible(true);
 	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -53,6 +60,7 @@ void startUp () {
 	}
 
 	randomize();
+	timer.start();
 }
 
 void randomize() {
@@ -60,16 +68,23 @@ void randomize() {
 	frame.add(panel);
 	Font f = new Font("Arial" , 0, 9);
 	for(int i = 0;i < 375;i += 1) {
-		
+		Icon image = new ImageIcon("Mole.jpg");
 		JButton button = new JButton();
 		button.setPreferredSize(new Dimension(50,50));
 		button.addActionListener(this);
 		int num = gen.nextInt(100);
 		if(num < 7){
 			button.setFont(f);
-			button.setText("Sus");
+			button.setIcon(image);
+			button.setText("");
+			
 			
 		}
+		
+		else {
+			button.setText(" ");
+		}
+		
 		panel.add(button);
 		
 	}
@@ -80,8 +95,16 @@ void randomize() {
 
 @Override
 public void actionPerformed(ActionEvent e) {
+	if(e.getSource()== timer) {
+		frame.setTitle("Time: " + seconds);
+	if (isOnEndState == false) {
+		seconds = seconds +1;
+	}
+	}
+	
+	else {
 	JButton clicked = (JButton) e.getSource();
-	if(clicked.getText().equals("Sus")) {
+	if(clicked.getText().equals("")) {
 		System.out.println("WHACK");
 		molesPressed = molesPressed +1;
 		frame.remove(panel);
@@ -91,16 +114,17 @@ public void actionPerformed(ActionEvent e) {
 	
 	else {
 		System.out.println("ERROR");
+		isOnEndState = true;
 		drawEndState(frame.getGraphics());
 		nonMolesPressed = nonMolesPressed +1;
 		frame.remove(panel);
 		panel = new JPanel();
 		frame.add(panel);
-		
-		// TO FIX: Keys Not Detected On End Screen
+		frame.requestFocus();
+	
 	}
 	
-	
+	}
 	
 }
 
@@ -117,7 +141,6 @@ public void drawEndState(Graphics g) {
 	g.setFont(font);
 	g.setColor(new Color(150, 250, 150));
 	g.drawString("Score:" + molesPressed + "", 50, 150);
-	// 0 is placeholder so there are no errors, there needs to be something done about it soon
 	g.setFont(fontEndScreen);
 	
 	g.setColor(new Color(150, 250, 150));
@@ -166,7 +189,9 @@ public void keyPressed(KeyEvent e) {
 	}
 	else {
 		frame.remove(panel);
+		isOnEndState = false;
 		randomize();
+		seconds = 0;
 	}
 	System.out.println(keyCode);
 	
